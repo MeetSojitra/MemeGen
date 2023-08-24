09db1369b1d7e0ca7f0366d7d4f5abfc94f61be5
Meet Sojitra <meetsojira2783@gmail.com>
2023-08-24 12:24:17 +0530

    Added memesurl

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import memesdata from "../memedata.jsx"
 
 export default function Meme() {
+     /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
+    function clicked(){
+        const memesarray=memesdata.data.memes;
+        // get a random image from the memesData array
+        const randomImage = Math.floor(Math.random() * memesarray.length);
+        const url = memesarray[randomImage].url;
+        console.log(url);
+    }
+
     return(
         <main>
             <form className="form">
@@ -14,7 +30,7 @@ export default function Meme() {
                     className="input"
                     placeholder="Bottom Text"
                 />
-                <button className="form--button">Get a new meme image 🖼</button>
+                <button onClick={clicked} className="form--button">Get a new meme image 🖼</button>
             </form>
             
             <div className="meme--image">
