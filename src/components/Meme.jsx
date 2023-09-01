ff2dc6161619f90afbf34101833b1ff68e3a5d96
Meet Sojitra <meetsojira2783@gmail.com>
2023-09-01 15:54:48 +0530

    Update

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,57 +1,55 @@
-import React from "react";
-import memesdata from "../memedata.jsx"
+import React from "react"
 
 export default function Meme() {
-     
-    /**
-     * Challenge: Update our state to save the meme-related
-     * data as an object called `meme`. It should have the
-     * following 3 properties:
-     * topText, bottomText, randomImage.
-     * 
-     * The 2 text states can default to empty strings for now,
-     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-     * 
-     * Next, create a new state variable called `allMemeImages`
-     * which will default to `memesData`, which we imported above
-     * 
-     * Lastly, update the `getMemeImage` function and the markup 
-     * to reflect our newly reformed state object and array in the
-     * correct way.
-     */
-
-    const[meme, setMeme] = React.useState({
-            topText: "",
-            bottomText: "",
-            randomImage: "http://i.imgflip.com/1bij.jpg"
-        });
-
-    const[allMemeImages, setAllMemeImages] = React.useState(memesdata);
-
-    function getMemeImage(){
-        const memesarray=memesdata.data.memes;
-        const randomImage = Math.floor(Math.random() * memesarray.length);
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemes, setAllMemes] = React.useState([])
+    
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
         
-        setMeme(prev => {
-            return {
-                ...prev,
-                randomImage: memesarray[randomImage].url
-            }
-        });
     }
-
-    return(
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
+    return (
         <main>
             <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="form--button"
@@ -60,10 +58,11 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
-
-            <img src={meme.randomImage} className="meme--image"/>
-            
-            
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
